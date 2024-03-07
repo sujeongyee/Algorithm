d7d499df09fb98c27a3dbd5c3fe91c3071af5c5f
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-03-08 02:24:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.92 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        List<Integer> arr2 = new ArrayList<>();
-		int a = -1;
+        Stack<Integer> stack = new Stack<>();
+		int n = 10;
 		for(int i = 0 ; i < arr.length ; i++) {
-			if(a!=arr[i]) {
-				a = arr[i];
-				arr2.add(arr[i]);
-			}
-		}		
-		int[] answer = arr2.stream().mapToInt(i->i).toArray();
+			if(arr[i]!=n) stack.push(arr[i]);
+			n=arr[i];
+		}	
 
-        return answer;
+        return stack.stream().mapToInt(i->i).toArray();
     }
 }

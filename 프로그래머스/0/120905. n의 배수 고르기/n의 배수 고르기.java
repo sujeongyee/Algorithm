c205630f12eed1c354181f7506e7d92bb62e52c4
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-03-11 15:39:46 +0900

    [level 0] Title: n의 배수 고르기, Time: 2.82 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = Arrays.stream(numlist).filter(i->i%n==0).toArray();
+        return answer;
+    }
+}

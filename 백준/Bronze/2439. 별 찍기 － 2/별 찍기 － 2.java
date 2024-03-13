8a51ca43934491d563a211f573002891ce85838c
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-03-13 23:56:39 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {		
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();		
+		for(int i = 1; i<=num ; i++) {
+			System.out.println(" ".repeat(num-i)+"*".repeat(i));
+		}
+		
+	}
+}

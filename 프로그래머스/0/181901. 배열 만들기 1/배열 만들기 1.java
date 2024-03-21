6baebfab80ac3c4d615c0b50873fcb29f1a6e2ed
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-03-21 13:44:36 +0900

    Update 배열 만들기 1.java (성능 UP)

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,11 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
+// class Solution {
+//     public int[] solution(int n, int k) {
+//         List<Integer> list = new ArrayList<>();
+// 		for(int i = k ; i <=n ; i+=k ) {
+// 			list.add(i);
+// 		}		
+//         return list.stream().mapToInt(i->i).toArray();
+//     }
+// }
+
+// 위에 풀이보다 성능 좋은 코드로 수정
+
 class Solution {
     public int[] solution(int n, int k) {
-        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[(n-k)/k+1];
+		int idx = 0;
 		for(int i = k ; i <=n ; i+=k ) {
-			list.add(i);
-		}		
-        return list.stream().mapToInt(i->i).toArray();
+			answer[idx] = i;
+			idx++;
+		}
+        return answer;
     }
 }

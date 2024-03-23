408952c5307c67a7e39b4bb89138ebc510e13391
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-03-23 23:54:06 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {				
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		try {
+			int a = Integer.parseInt(br.readLine());
+			int b = Integer.parseInt(br.readLine());
+			System.out.println(a+b);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}	
+}

ab970287dfc9b73f406bbfd834f4e64777fd4447
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-04-23 22:25:56 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 9.03 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        List<Integer> list = new ArrayList<>();
+		for(int i = 0 ; i < arr.length ; i++) {
+			if(!list.contains(arr[i])) {
+				list.add(arr[i]);
+			}
+			if(list.size() == k) break;
+		}
+		
+		if(list.size()!=k) {
+			for(int i = list.size() ; i < k ; i++) {
+				list.add(-1);
+			}
+		}
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}

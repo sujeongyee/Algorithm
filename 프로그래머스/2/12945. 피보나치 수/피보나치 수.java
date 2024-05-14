6d6a3bf3dba6cb498d523f3f178fab2425af754f
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-05-15 03:08:43 +0900

    [level 2] Title: 피보나치 수, Time: 1.74 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,15 +1,15 @@
-import java.math.BigInteger;
+
 class Solution {
     public int solution(int n) {
-       BigInteger[] arr = {BigInteger.valueOf(0),BigInteger.valueOf(1)};
-		BigInteger pibo = BigInteger.valueOf(0);
-		for(int i = 2 ; i <=n ; i++) {
-			pibo = arr[0].add(arr[1]);
-			arr[0] = arr[1];
-			arr[1] = pibo;
-		}
-		BigInteger answer = pibo.remainder(BigInteger.valueOf(1234567) );
-		int an = Integer.parseInt( answer.toString());
-        return an;
+        int answer = 1;
+        int f1 = 0, f2 = 1;
+
+        for(int i = 2; i<=n; i++){
+            answer = (f1 + f2)%1234567;
+            f1 = f2;
+            f2 = answer;
+        }
+
+        return answer;
     }
 }

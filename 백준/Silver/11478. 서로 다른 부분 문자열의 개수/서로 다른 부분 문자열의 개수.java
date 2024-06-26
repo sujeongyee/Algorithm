4e43a0ddb76145b2e96a25c2dac5c6830996c33c
박수정 <133090100+sujeongyee@users.noreply.github.com>
2024-06-26 17:50:52 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 744 ms, Memory: 227308 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {    
+    public static void main(String[] args) {        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+        
+        try {
+        	String s = br.readLine();
+        	Set<String> set = new HashSet<String>();
+        	
+        	for(int i = 0 ; i < s.length() ; i++) {
+        		for(int j = i+1 ; j <= s.length() ; j++) {
+        			set.add(s.substring(i,j));
+        		}
+        	}
+            
+            System.out.println(set.size());
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }        
+    }
+}
